dc98015051e68b6f72f16357e9cb484245b0cb89
rodri542 <l20120987@morelia.tecnm.mx>
2023-05-10 22:24:14 -0600

    Cambios Basicos

---
+++ lib/pages/sing_in_page.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+class SingInPage extends StatefulWidget {
+  const SingInPage({super.key});
+
+  @override
+  State<SingInPage> createState() => _SingInPageState();
+}
+
+class _SingInPageState extends State<SingInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+
+          ],
+          
+        ),
+
+
+    );
+  }
+}
---
+++ lib/pages/sing_up_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SingUpPage extends StatelessWidget {
+  const SingUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      
+    );
+  }
+}
